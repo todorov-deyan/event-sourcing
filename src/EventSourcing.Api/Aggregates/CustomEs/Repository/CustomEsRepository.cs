79e99f5067d1fafd61407e8483fbfa87e13365a5
deyantodorov <deyan.todorov@endava.com>
2022-11-16 13:54:10 +0200

    add customes repository

---
+++ src/EventSourcing.Api/Aggregates/CustomEs/Repository/CustomEsRepository.cs
@@ -0,0 +1,17 @@
+﻿using EventSourcing.Api.Common.EventSourcing;
+
+namespace EventSourcing.Api.Aggregates.CustomEs.Repository
+{
+    public class CustomEsRepository<T> : ICustomEsRepository<T> where T : class, IAggregate
+    {
+        public Task Update(Guid id, IList<IEventState> events, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Add(T aggregate, IList<IEventState> events, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
