dcd7ccc5718b93b8f32b5f0b6b6304e7bf218215
Biser Borisov <Biser.Borisov@endava.com>
2022-11-23 11:01:39 +0200

    Change Tests to async

--- src/EventSourcing.Tests/MartenDb/MartenDBPostgreeTest.cs
+++ src/EventSourcing.Tests/MartenDb/MartenDBPostgreeTest.cs
@@ -39,7 +39,7 @@ namespace EventSourcing.Tests.MartenDb
 
 
         [Fact, Order(2)]
-        public void CreateAccount()
+        public async void CreateAccount()
         {
             var account = new Account
             {
@@ -55,10 +55,10 @@ namespace EventSourcing.Tests.MartenDb
                 Description = "Saved money"
             };
 
-            _repository.Add(account, new List<IEventState> { createEvent }, default);
-            var result = _repository.Find(account.Id, CancellationToken.None);
+            await _repository.Add(account, new List<IEventState> { createEvent }, default);
+            var result = await _repository.Find(account.Id, CancellationToken.None);
 
-            Assert.NotNull(result.Result);
+            Assert.NotNull(result);
         }
 
         [Fact, Order(3)]
@@ -67,7 +67,7 @@ namespace EventSourcing.Tests.MartenDb
         }
 
         [Fact, Order(4)]
-        public void ActivateAccount()
+        public async void ActivateAccount()
         {
             Guid streamId = new Guid("5d0b0dbf-365b-4fe0-85c4-c6a670a934cb");
 
@@ -77,14 +77,14 @@ namespace EventSourcing.Tests.MartenDb
                 Description = "Saved money. Activated"
             };
 
-            _repository.Update(streamId, new List<IEventState> { createEvent }, default);
-            var result = _repository.Find(streamId, CancellationToken.None);
+            await _repository.Update(streamId, new List<IEventState> { createEvent }, default);
+            var result = await _repository.Find(streamId, CancellationToken.None);
 
-            Assert.NotNull(result.Result);
+            Assert.NotNull(result);
         }
 
         [Fact, Order(5)]
-        public void DeactivateAccount()
+        public async void DeactivateAccount()
         {
             Guid streamId = new Guid("5d0b0dbf-365b-4fe0-85c4-c6a670a934cb");
 
@@ -95,26 +95,21 @@ namespace EventSourcing.Tests.MartenDb
                 Description = "Saved money. Deactivated"
             };
 
-            _repository.Update(streamId, new List<IEventState> { createEvent }, default);
-            var result = _repository.Find(streamId, CancellationToken.None);
+            await _repository.Update(streamId, new List<IEventState> { createEvent }, default);
+            var result = await _repository.Find(streamId, CancellationToken.None);
 
-            Assert.NotNull(result.Result);
+            Assert.NotNull(result);
         }
 
         [Fact, Order(6)]
-        public void GetAccount_ById()
+        public async void GetAccount_ById()
         {
             Guid streamId = new Guid("5d0b0dbf-365b-4fe0-85c4-c6a670a934cb");
-            var result = _repository.Find(streamId, CancellationToken.None);
+            var result = await _repository.Find(streamId, CancellationToken.None);
 
-            Assert.NotNull(result.Result);
+            Assert.NotNull(result);
         }
 
 
-        [Fact, Order(7)]
-        public void GetAccountAll_ById_Reflection()
-        {
-
-        }
     }
 }
